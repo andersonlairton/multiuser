0a40dfac9c032188581cda9feb092b710bb2751c
Anderson Lairton <lairton.anderson@gmail.com>
2020-01-08 22:05:16 -0300

    Adicionando a tabela Administrador ,onde foi inserido dados do usuario,como no exemplo da devmedia

---
+++ protected/views/administrador/_search.php
@@ -0,0 +1,50 @@
+<?php
+/* @var $this AdministradorController */
+/* @var $model Administrador */
+/* @var $form CActiveForm */
+?>
+
+<div class="wide form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
+)); ?>
+
+	<div class="row">
+		<?php echo $form->label($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'nivel'); ?>
+		<?php echo $form->textField($model,'nivel'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'nome'); ?>
+		<?php echo $form->textField($model,'nome',array('size'=>60,'maxlength'=>300)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>300)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'usuario'); ?>
+		<?php echo $form->textField($model,'usuario',array('size'=>60,'maxlength'=>300)); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->label($model,'senha'); ?>
+		<?php echo $form->textField($model,'senha',array('size'=>60,'maxlength'=>300)); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton('Search'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- search-form -->
---
+++ protected/views/administrador/_view.php
@@ -0,0 +1,33 @@
+<?php
+/* @var $this AdministradorController */
+/* @var $data Administrador */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nivel')); ?>:</b>
+	<?php echo CHtml::encode($data->nivel); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nome')); ?>:</b>
+	<?php echo CHtml::encode($data->nome); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('email')); ?>:</b>
+	<?php echo CHtml::encode($data->email); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('usuario')); ?>:</b>
+	<?php echo CHtml::encode($data->usuario); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('senha')); ?>:</b>
+	<?php echo CHtml::encode($data->senha); ?>
+	<br />
+
+
+</div>
